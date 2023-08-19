ca19a875b147771144147f1d5fd62e17bc42e585
FilKarnicki <filip.karnicki@gmail.com>
2023-08-19 20:00:26 +0200

    cats eval - also forced, just trying it out

--- client/src/main/scala/eu/karnicki/fun/async/Pricing.scala
+++ client/src/main/scala/eu/karnicki/fun/async/Pricing.scala
@@ -8,9 +8,28 @@ import scala.util.{Failure, Success}
 object Pricing:
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
+  private def getRate: BigDecimal = BigDecimal(scala.util.Random.nextDouble * 10)
+
+  private def nowPremium(rate: BigDecimal) = cats.Eval.now(rate * scala.util.Random.nextDouble)
+
+  // some made up logic
   def priceAsync(notional: BigDecimal)(onSuccess: BigDecimal => Unit)(onFailure: Throwable => Unit): Unit =
     Future {
-      notional / 100
+      val oneRate = cats.Eval.later {
+        println(s"getting rate once for $notional")
+        getRate
+      }
+
+      val price = for {
+        rate <- oneRate
+      } yield rate * notional / 100
+
+      val premium = for {
+        rate <- oneRate
+        premium <- nowPremium(rate)
+      } yield premium
+
+      price.value + premium.value
     }.onComplete {
       case Failure(exception) =>
         onFailure(exception)
