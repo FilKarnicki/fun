66b05822750ea20b46ef731dc83926e602a98316
FilKarnicki <filip.karnicki@gmail.com>
2023-08-20 07:46:05 +0200

    cats eval - messing around with memoization

--- client/src/main/scala/eu/karnicki/fun/async/Pricing.scala
+++ client/src/main/scala/eu/karnicki/fun/async/Pricing.scala
@@ -3,33 +3,36 @@ package eu.karnicki.fun.async
 import zio.*
 
 import scala.concurrent.Future
+import scala.math.BigDecimal.RoundingMode
 import scala.util.{Failure, Success}
 
 object Pricing:
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
-  private def getRate: BigDecimal = BigDecimal(scala.util.Random.nextDouble * 10)
+  private def getRate: BigDecimal =
+    println(s"Calling a service to get a rate")
+    Thread.sleep(1000)
+    BigDecimal(scala.util.Random.nextDouble * 10)
 
   private def nowPremium(rate: BigDecimal) = cats.Eval.now(rate * scala.util.Random.nextDouble)
 
   // some made up logic
   def priceAsync(notional: BigDecimal)(onSuccess: BigDecimal => Unit)(onFailure: Throwable => Unit): Unit =
     Future {
-      val oneRate = cats.Eval.later {
-        println(s"getting rate once for $notional")
-        getRate
-      }
+      val priceLeg = cats.Eval.always(getRate)
+        .map(rate =>
+          rate * notional / 1_000)
+        .memoize
+        .map(premium =>
+          (premium + scala.util.Random.nextDouble).setScale(4, RoundingMode.UP))
 
-      val price = for {
-        rate <- oneRate
-      } yield rate * notional / 100
+      // called twice but rate only retrieved once
+      val priceLegsWithSameRate = for {
+        priceLeg1 <- priceLeg
+        priceLeg2 <- priceLeg
+      } yield priceLeg1 + priceLeg2 * -1
 
-      val premium = for {
-        rate <- oneRate
-        premium <- nowPremium(rate)
-      } yield premium
-
-      price.value + premium.value
+      priceLegsWithSameRate.value
     }.onComplete {
       case Failure(exception) =>
         onFailure(exception)
