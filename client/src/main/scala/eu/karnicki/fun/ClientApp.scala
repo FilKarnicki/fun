7714bb0d5ffb5fce2c8642dbba05dd69b357c187
FilKarnicki <filip.karnicki@gmail.com>
2023-08-21 16:42:40 +0200

    adding unnecessary cats State for creating contracts

--- client/src/main/scala/eu/karnicki/fun/ClientApp.scala
+++ client/src/main/scala/eu/karnicki/fun/ClientApp.scala
@@ -12,29 +12,53 @@ import zio.http.netty.NettyConfig
 import zio.http.netty.client.NettyClientDriver
 import zio.http.{Client, DnsResolver, Response, ZClient}
 
+import java.util.UUID
 import scala.io.Source
 import scala.language.{existentials, postfixOps}
 
 object ClientApp extends ZIOAppDefault:
   def effects(eventSource: IO[Throwable, Seq[Event]]): ZIO[Client with CounterpartyServiceConfig with CounterpartyService,
     Throwable,
-    Seq[EnrichedEvent]] =
+    Seq[Contract]] =
     for {
       counterpartyService <- ZIO.service[CounterpartyService]
       counterpartyServiceConfig <- ZIO.service[CounterpartyServiceConfig]
       counterpartyServiceSemaphore <- Semaphore.make(counterpartyServiceConfig.maxInFlight)
       events <- eventSource
       pricedEventsAndCounterparties <- priceEventsAndGetCounterparties(counterpartyService, counterpartyServiceSemaphore, events)
+      enrichedEvents = mapPricedEventsAndCounterpartiesToEnrichedEvents(pricedEventsAndCounterparties)
+      contracts = createContracts(enrichedEvents)
+    } yield contracts
 
-      enrichedEvents = pricedEventsAndCounterparties.map {
-        case (event, price, seq) =>
-          seq match
-            case (ClientSide.Buyer, client) :: tail =>
-              EnrichedEvent(event, client, tail.head._2, price)
-            case (ClientSide.Seller, client) :: tail =>
-              EnrichedEvent(event, tail.head._2, client, price)
-      }
-    } yield enrichedEvents
+  private def createContracts(enrichedEvents: Seq[EnrichedEvent]) = {
+    enrichedEvents.map { enrichedEvent =>
+      (for {
+        premium <- cats.data.State[Contract, Instrument](
+          contract =>
+            val premium = Instrument(UUID.randomUUID.toString, enrichedEvent.price)
+            val buyerObligation = Obligation(enrichedEvent.seller, enrichedEvent.buyer, Seq(premium), Status.live)
+
+            (contract.copy(obligations = buyerObligation +: contract.obligations), premium))
+        otherLeg <- cats.data.State[Contract, Instrument](
+          contract =>
+            val otherLeg = Instrument(UUID.randomUUID.toString, enrichedEvent.event.notional)
+            val sellerObligation = Obligation(enrichedEvent.seller, enrichedEvent.buyer, Seq(otherLeg), Status.live)
+
+            (contract.copy(obligations = sellerObligation +: contract.obligations), otherLeg))
+      } yield otherLeg).run(Contract(UUID.randomUUID.toString, Nil)).value._1
+    }
+  }
+
+  private def mapPricedEventsAndCounterpartiesToEnrichedEvents(pricedEventsAndCounterparties: Seq[(Event, BigDecimal, Seq[(ClientSide, _root_.java.lang.String)])]) = {
+    pricedEventsAndCounterparties.map {
+      case (event, price, seq) =>
+        seq match
+          case (ClientSide.Buyer, client) :: tail =>
+            EnrichedEvent(event, client, tail.head._2, price)
+          case (ClientSide.Seller, client) :: tail =>
+            EnrichedEvent(event, tail.head._2, client, price)
+    }
+  }
 
   private def priceEventsAndGetCounterparties(counterpartyService: CounterpartyService, counterpartyServiceSemaphore: Semaphore, events: Seq[Event]) = {
     ZIO.collectAll(
